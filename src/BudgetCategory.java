f17b27e79089bfbf2d0b38ebc33cb91927bd5941
Kelley Castillo <kelleycastillo@Kelleys-MacBook-Pro.local>
2025-04-23 11:46:05 -0700

    added stuff

--- src/BudgetCategory.java
+++ src/BudgetCategory.java
@@ -1,3 +1,29 @@
 public class BudgetCategory {
+        private double limit;
+        private double spending;
+        private String category;
     
+        public BudgetCategory(String category, double limit, double spending) {
+            this.category = category;
+            this.limit = limit;
+            this.spending = spending;
+        }
+    
+        public double getLimit() {
+            return limit;
+        }
+    
+        public double getSpending() {
+            return spending;
+        }
+    
+        public String getCategory() {
+            return category;
+        }
+    
+        @Override
+        public String toString() {
+            return limit + "" + spending + "" + category;
+        }
+    }
 }
