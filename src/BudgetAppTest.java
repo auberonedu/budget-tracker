58f7bb07907722792a7dce972d9401f30e9f23ba
Akai <161771111+Akaiokaa@users.noreply.github.com>
2025-04-23 16:08:24 -0700

    Finished wave 2 testing toString is complete

--- src/BudgetAppTest.java
+++ src/BudgetAppTest.java
@@ -2,5 +2,16 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BudgetAppTest {
-    
+    @Test
+    void testToString(){
+        //Arrange
+        BudgetCategory budget = new BudgetCategory("Food", 400, 500);
+
+        //Act
+        String actual = budget.toString();
+
+        //Assert
+        String expected = "Category: Food" + "\nlimit: 400.0" + "\nspent: 500.0";
+        assertEquals(expected, actual);
+    }
 }
