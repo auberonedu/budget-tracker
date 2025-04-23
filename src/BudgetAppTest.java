9e76291e05d3d65db8baea1e39efc228e8040a8a
kriseattle <notbbbfamous@gmail.com>
2025-04-23 13:29:49 -0700

    Added code to complete wave 2 with BudgetAppTest.java

--- src/BudgetAppTest.java
+++ src/BudgetAppTest.java
@@ -2,5 +2,31 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BudgetAppTest {
+
+    // Wave 2
+    @Test
+    void testingToStringForRent(){
+        // Arrange
+       BudgetCategory rent = new BudgetCategory("rent", 500.0, 500.0);
+       String expected = "The budget limit for rent was: 500.0 but the actual spend was 500.0";
+
+        // Act
+        String actual = rent.toString();
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testingToStringForEntertainment(){
+        // Arrange
+        BudgetCategory entertainment = new BudgetCategory("entertainment", 125, 150);
+        String expected = "The budget limit for entertainment was: 125.0 but the actual spend was 150.0";
+
+        // Act
+        String actual = entertainment.toString();
+
+        // Assert
+        assertEquals(expected, actual);
+    }
     
 }
