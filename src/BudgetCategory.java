29a75e6e6357ab231077f1ee1e52f86dfb635746
Gulyapas Poonkawinsiri <67386394+joetlobb@users.noreply.github.com>
2025-04-23 11:31:50 -0700

    add getter and toString methods

--- src/BudgetCategory.java
+++ src/BudgetCategory.java
@@ -1,3 +1,22 @@
 public class BudgetCategory {
+    private String category;
+    private double limit;
+    private double spending;
     
+    public String getCategory() {
+        return category;
+    }
+
+    public double getLimit() {
+        return limit;
+    }
+
+    public double getSpending() {
+        return spending;
+    }
+
+    @Override
+    public String toString() {
+        return "BudgetCategory [category=" + category + ", limit=" + limit + ", spending=" + spending + "]";
+    }
 }
