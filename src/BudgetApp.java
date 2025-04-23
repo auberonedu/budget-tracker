8c298fe0b08e87a12feb16cb080dfb430ffb9f8f
Iquer_Dough <206500499+DennysSaenz123@users.noreply.github.com>
2025-04-23 11:44:22 -0700

    Created BudgetCategory class and added condtructor and toString method

--- src/BudgetApp.java
+++ src/BudgetApp.java
@@ -5,19 +5,23 @@ public class BudgetApp {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        BudgetCategory groceries = new BudgetCategory("Groceries", 500, 401);
+
+        System.out.println(groceries.toString());
+
         while(scan.hasNextLine()) {
             String category = scan.nextLine();
 
             double limit = scan.nextDouble();
             double spent = scan.nextDouble();
 
-            // Consume \n after spent input 
+            //Consume \n after spent input 
             if(scan.hasNextLine()) scan.nextLine();
 
-            String limitString = String.format("$%.2f", limit);
-            String spentString = String.format("$%.2f", spent);
-            System.out.println("The budget limit for " + category + " was: " + limitString + 
-                               " but the actual spend was " + spentString);
+            // String limitString = String.format("$%.2f", limit);
+            // String spentString = String.format("$%.2f", spent);
+            // System.out.println("The budget limit for " + category + " was: " + limitString + 
+            //                    " but the actual spend was " + spentString);
         }
     }
 
