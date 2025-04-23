d6e4d9e302a4ef7733414295927e3c425dbe67e3
Akai <161771111+Akaiokaa@users.noreply.github.com>
2025-04-23 15:50:48 -0700

    Wave 1 constructors and getter done

--- src/BudgetCategory.java
+++ src/BudgetCategory.java
@@ -1,3 +1,28 @@
 public class BudgetCategory {
+    private String category;
+    private double limit;
+    private double spent;
     
+    public String getCategory() {
+        return category;
+    }
+
+    public double getLimit() {
+        return limit;
+    }
+
+    public double getSpent() {
+        return spent;
+    }
+
+    public BudgetCategory(String category, double limit, double spent){
+        this.category = category;
+        this.limit = limit;
+        this.spent = spent;
+    }
+
+    @Override
+    public String toString(){
+        return "Category: " + category + "\nlimit: " + limit + "\nspent: " + spent;
+    }
 }
