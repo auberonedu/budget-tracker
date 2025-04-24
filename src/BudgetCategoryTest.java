cb41cd6c5ceb09a3e611691002c5ff296baa3d21
Kelley Castillo <kelleycastillo@Kelleys-MacBook-Pro.local>
2025-04-24 10:58:08 -0700

    edited more

--- src/BudgetCategoryTest.java
+++ src/BudgetCategoryTest.java
@@ -4,23 +4,15 @@ import static org.junit.jupiter.api.Assertions.*;
 public class BudgetCategoryTest {
 
     @Test
-    void testCompareTo() {
-        
+    void testToString() {
+        // Arrange
+        BudgetCategory category = new BudgetCategory("Groceries", 500.00, 401.00);
+
+        // Act
+        String actual = category.toString();
+
+        // Assert 
+        String expected = "Category: Groceries, Limit: $500.0, Actual: $401.0, Difference: $50.0";
+        assertEquals(expected, actual);
     }
-
-    @Test
-    void testGetCategory() {
-        
-    }
-
-    @Test
-    void testGetDiff() {
-        
-    }
-
-    @Test
-    void testGetLimit() {
-        
-    }}
-
-    
+}
