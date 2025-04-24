46322d33dbf1e67a2305e45b64581f0f95358bd0
Kelley Castillo <kelleycastillo@Kelleys-MacBook-Pro.local>
2025-04-24 11:11:06 -0700

    added 3 tests

--- src/BudgetCategoryTest.java
+++ src/BudgetCategoryTest.java
@@ -6,7 +6,7 @@ public class BudgetCategoryTest {
     @Test
     void testToString() {
         // Arrange
-        BudgetCategory category = new BudgetCategory("Groceries", 500.00, 401.00);
+        BudgetCategory category = new BudgetCategory("Groceries", 500.00, 401.00,);
 
         // Act
         String actual = category.toString();
@@ -15,4 +15,17 @@ public class BudgetCategoryTest {
         String expected = "Category: Groceries, Limit: $500.0, Actual: $401.0, Difference: $50.0";
         assertEquals(expected, actual);
     }
+
+    @Test
+    void testCompareToSameDiff() {
+        //Arrange
+        BudgetCategory groceries = new BudgetCategory("Groceries", 500, 450); 
+        BudgetCategory utilities = new BudgetCategory("Utilities", 300, 250); 
+
+          // Act: Comparing the two categories
+          int spending = groceries.compareTo(utilities);
+
+          // Assert: 
+          assertEquals(0, spending); 
+      }
 }
