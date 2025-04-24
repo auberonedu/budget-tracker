b5a44e7dcd299f006a7d437e2fb3ae513571267d
Gulyapas Poonkawinsiri <67386394+joetlobb@users.noreply.github.com>
2025-04-24 10:06:09 -0700

    refactor BudgetApp, add item to myBudgetList and print the list

--- src/BudgetApp.java
+++ src/BudgetApp.java
@@ -1,33 +1,51 @@
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class BudgetApp {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        // BudgetCategory groceries = new BudgetCategory("Groceries", 500, 401);
+        // BudgetCategory rent = new BudgetCategory("Rent", 1500, 1500);
 
-        while(scan.hasNextLine()) {
+        // System.out.println(groceries.toString());
+
+        Scanner scan = new Scanner(System.in);
+        List<BudgetCategory> myBudgetList = new ArrayList<>();
+
+        while (scan.hasNextLine()) {
             String category = scan.nextLine();
 
             double limit = scan.nextDouble();
             double spent = scan.nextDouble();
 
-            // Consume \n after spent input 
-            if(scan.hasNextLine()) scan.nextLine();
+            // Consume \n after spent input
+            if (scan.hasNextLine())
+                scan.nextLine();
 
-            String limitString = String.format("$%.2f", limit);
-            String spentString = String.format("$%.2f", spent);
-            System.out.println("The budget limit for " + category + " was: " + limitString + 
-                               " but the actual spend was " + spentString);
+            String limitString = String.format("%.2f", limit);
+            String spentString = String.format("%.2f", spent);
+
+            BudgetCategory budget = new BudgetCategory(category, Double.parseDouble(limitString),
+                    Double.parseDouble(spentString));
+
+            myBudgetList.add(budget);
+            // System.out.println("The budget limit for " + category + " was: " +
+            // limitString +
+            // " but the actual spend was " + spentString);
         }
+
+        System.out.println(myBudgetList);
     }
 
     /**
      * Returns overall how much over/under budget a person is given a list of their
      * categories.
      * 
-     * This should be the sum of how much over/under budget each individual category is.
+     * This should be the sum of how much over/under budget each individual category
+     * is.
      * 
-     * If the person is under budget, the result will be negative. If they are over budget, the
+     * If the person is under budget, the result will be negative. If they are over
+     * budget, the
      * result will be positive.
      * 
      * @param categories the budget categories with the spend
