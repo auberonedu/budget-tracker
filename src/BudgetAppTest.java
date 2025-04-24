40a13c526b9e91cf71fe00ddc3fd577fd3a1db8a
Gulyapas Poonkawinsiri <67386394+joetlobb@users.noreply.github.com>
2025-04-24 10:39:31 -0700

    add 3 test cases for compareTo method

--- src/BudgetAppTest.java
+++ src/BudgetAppTest.java
@@ -14,4 +14,43 @@ public class BudgetAppTest {
         // Assert
         assertEquals(string, actual);
     }
+
+    @Test
+    void testDescribeBudgetSavingsPositive() {
+        // Arrange
+        BudgetCategory groceries = new BudgetCategory("Groceries", 500, 401);
+        BudgetCategory tvShowSub = new BudgetCategory("TVShowSubscription", 100, 100);
+
+        // Act
+        int actual = groceries.compareTo(tvShowSub);
+
+        // Assert
+        assertEquals(1, actual);
+    }
+
+    @Test
+    void testDescribeBudgetSavingsNegative() {
+        // Arrange
+        BudgetCategory rent = new BudgetCategory("Rent", 1500, 1500);
+        BudgetCategory tvShowSub = new BudgetCategory("TVShowSubscription", 100, 80);
+
+        // Act
+        int actual = rent.compareTo(tvShowSub);
+
+        // Assert
+        assertEquals(-1, actual);
+    }
+
+    @Test
+    void testDescribeBudgetSavingsAreZero() {
+        // Arrange
+        BudgetCategory utilities = new BudgetCategory("Utilities", 350, 270);
+        BudgetCategory transportation = new BudgetCategory("Transportation", 500, 420);
+
+        // Act
+        int actual = utilities.compareTo(transportation);
+
+        // Assert
+        assertEquals(0, actual);
+    }
 }
