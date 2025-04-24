43374c8ff2c6e00a39f0e9455337c2f69dd8eab9
Gulyapas Poonkawinsiri <67386394+joetlobb@users.noreply.github.com>
2025-04-24 09:37:37 -0700

    add testDescribeBudgetCategoryToString

--- src/BudgetAppTest.java
+++ src/BudgetAppTest.java
@@ -2,5 +2,16 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BudgetAppTest {
-    
+    @Test
+    void testDescribeBudgetCategoryToString() {
+        // Arrange
+        String string = "BudgetCategory : Groceries, limit=500.0, spending=401.0";
+        BudgetCategory groceries = new BudgetCategory("Groceries", 500, 401);
+        
+        // Act
+        String actual = groceries.toString();
+        
+        // Assert
+        assertEquals(string, actual);
+    }
 }
