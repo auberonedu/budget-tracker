343bff780140c53363f506fe84d7685a6ea317c6
Kelley Castillo <kelleycastillo@Kelleys-MacBook-Pro.local>
2025-04-24 10:28:49 -0700

    Created tests

--- src/BudgetCategoryTest.java
+++ src/BudgetCategoryTest.java
@@ -1,4 +1,26 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class BudgetCategoryTest {}
+public class BudgetCategoryTest {
+
+    @Test
+    void testCompareTo() {
+        
+    }
+
+    @Test
+    void testGetCategory() {
+        
+    }
+
+    @Test
+    void testGetDiff() {
+        
+    }
+
+    @Test
+    void testGetLimit() {
+        
+    }}
+
+    
