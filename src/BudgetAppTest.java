c2f722cc0d74c01d31e5a0969c99ea020bf907a9
Johncarlo Perez <johncarloperez@Johncarlos-MacBook-Pro.local>
2025-04-25 07:29:42 -0700

    added testingToString method to test for toString results

--- src/BudgetAppTest.java
+++ src/BudgetAppTest.java
@@ -3,4 +3,17 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class BudgetAppTest {
     
+    @Test
+    void testingToString() {
+        String category = "Entertainment";
+        double limit = 100.0;
+        double spent = 50.0;
+        
+        BudgetCategory category2 = new BudgetCategory(category, limit, spent);
+        String expected = category + " " + limit + " " + spent;
+
+        String acutal = category2.toString();
+
+        assertEquals(expected, acutal);
+    }
 }
