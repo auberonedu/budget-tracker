7e7c6a95d44d2aae56dbbdb2d0225d48987bc4f6
konradkelly <konradky@gmail.com>
2025-04-25 21:25:44 -0700

    added unit-tests for toString method

--- src/BudgetAppTest.java
+++ src/BudgetAppTest.java
@@ -5,5 +5,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BudgetAppTest {
+
+    @Test
+    public void testToString() {
+        BudgetCategory groceries = new BudgetCategory("groceries", 250, 200);
+            String actual = "Category: groceries\n"
+            + "Limit: 250.0\n"
+            + "Actual: 200.0";
+        assertEquals(actual, groceries.toString());
+    }
+    @Test
+    public void testToStringWhenCategoryFieldIsBlank() {
+        BudgetCategory groceries = new BudgetCategory("", 0.0, 0.0);
+            String actual ="Category: \n"
+            + "Limit: 0.0\n"
+            + "Actual: 0.0";
+        assertEquals(actual, groceries.toString());
+    }
 }
 
