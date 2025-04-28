e4794abddbe41696e6caedd7103579652ad2db65
Masih <masihrahmani87@gmail.com>
2025-04-27 17:45:15 -0700

    I run some test on my toString method and my method pass the test ok.

--- src/BudgetAppTest.java
+++ src/BudgetAppTest.java
@@ -2,5 +2,16 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BudgetAppTest {
+
+  @Test
+    public void testToString() {
+        BudgetCategory groceries = new BudgetCategory("Groceries", 500, 401);
+
+        String actual = groceries.toString();
+
+        String expected = "Category: Groceries spent: 401.0 limit: 500.0";
+        assertEquals(expected, actual);
+    }
+
     
 }
