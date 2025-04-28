e2b70f457d19cb45f621b1ad0ffdb93578e32b65
Iquer_Dough <206500499+DennysSaenz123@users.noreply.github.com>
2025-04-28 08:49:07 -0700

    Added test for toString method

--- src/BudgetAppTest.java
+++ src/BudgetAppTest.java
@@ -2,5 +2,16 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BudgetAppTest {
+    @Test
+    void toStringTrue(){
+        BudgetCategory groceries = new BudgetCategory("Groceries", 200, 50);
+
+        String actual = groceries.toString();
+
+        String expected = "Groceries 200.0 50.0";
+
+        assertEquals(expected, actual);
+
+    }
     
 }
