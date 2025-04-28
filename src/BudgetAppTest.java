95463cf604e2a0a0c34142e385e0a566a80d48db
sandeersson <sandra.anderstrom@gmail.com>
2025-04-27 21:08:18 -0700

    Wrote test statements for budgetDifference

--- src/BudgetAppTest.java
+++ src/BudgetAppTest.java
@@ -1,6 +1,9 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BudgetAppTest {
     @Test
     public void testBudgetCategoryToString() {
@@ -27,8 +30,8 @@ public class BudgetAppTest {
     @Test
     public void testCompareToGreaterThan() {
         // Arrange
-        BudgetCategory eatingOut = new BudgetCategory("Eating Out", 200.0, 250.0); // +50
-        BudgetCategory groceries = new BudgetCategory("Groceries", 500.0, 450.0);  // -50
+        BudgetCategory eatingOut = new BudgetCategory("Eating Out", 200.0, 250.0);
+        BudgetCategory groceries = new BudgetCategory("Groceries", 500.0, 450.0);
 
         // Act
         int actual = eatingOut.compareTo(groceries);
@@ -40,13 +43,55 @@ public class BudgetAppTest {
     @Test
     public void testCompareToEqual() {
         // Arrange
-        BudgetCategory groceries1 = new BudgetCategory("Groceries", 500.0, 450.0); // -50
-        BudgetCategory groceries2 = new BudgetCategory("Groceries", 300.0, 250.0); // -50
+        BudgetCategory groceries1 = new BudgetCategory("Groceries", 500.0, 450.0);
+        BudgetCategory groceries2 = new BudgetCategory("Groceries", 300.0, 250.0);
 
         // Act
         int actual = groceries1.compareTo(groceries2);
 
         // Assert
         assertEquals(0, actual);
+    }
+
+     @Test
+    public void testBudgetDifferenceAllUnderBudget() {
+        // Arrange
+        List<BudgetCategory> categories = new ArrayList<>();
+        categories.add(new BudgetCategory("Groceries", 500.0, 450.0));
+        categories.add(new BudgetCategory("Entertainment", 300.0, 250.0));
+
+        // Act
+        int actual = BudgetApp.budgetDifference(categories);
+
+        // Assert
+        assertEquals(-100, actual);
+    }
+
+    @Test
+    public void testBudgetDifferenceMixed() {
+        // Arrange
+        List<BudgetCategory> categories = new ArrayList<>();
+        categories.add(new BudgetCategory("Groceries", 500.0, 550.0));
+        categories.add(new BudgetCategory("Entertainment", 300.0, 250.0));
+
+        // Act
+        int actual = BudgetApp.budgetDifference(categories);
+
+        // Assert
+        assertEquals(0, actual);
+    }
+
+    @Test
+    public void testBudgetDifferenceAllOverBudget() {
+        // Arrange
+        List<BudgetCategory> categories = new ArrayList<>();
+        categories.add(new BudgetCategory("Dining", 200.0, 300.0));
+        categories.add(new BudgetCategory("Rent", 1000.0, 1050.0));
+
+        // Act
+        int actual = BudgetApp.budgetDifference(categories);
+
+        // Assert
+        assertEquals(150, actual);
     }
 }
