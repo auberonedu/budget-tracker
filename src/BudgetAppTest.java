e8ea93c931d067f3772364bed58de9f0bf57c1b9
sandeersson <sandra.anderstrom@gmail.com>
2025-04-27 20:30:39 -0700

    Wrote a test method in BudgetAppTest

--- src/BudgetAppTest.java
+++ src/BudgetAppTest.java
@@ -2,5 +2,14 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BudgetAppTest {
-    
+    @Test
+    public void testBudgetCategoryToString() {
+        // Arrange
+        BudgetCategory category = new BudgetCategory("Entertainment", 300.0, 500.0);
+        // Act
+        String actual = category.toString();
+        // Assert
+        String expected = "Entertainment 300.0 500.0";
+        assertEquals(expected, actual);
+    }
 }
