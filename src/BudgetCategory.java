6952b1762a6ef57cb06177513dc95548311cc7dd
Masih <masihrahmani87@gmail.com>
2025-04-27 17:29:47 -0700

    I am done with wave one I have added the neccecery codes for BudgetCategory class.

--- src/BudgetCategory.java
+++ src/BudgetCategory.java
@@ -1,3 +1,31 @@
 public class BudgetCategory {
-    
+  private String category;
+  private double limit;
+  private double spent;
+  
+  public BudgetCategory(String category, double limit , double spent){
+    this.limit = limit;
+    this.spent = spent;
+    this.category = category;
+
+
+  }
+
+  public double getLimit() {
+    return limit;
+  }
+
+  public double getSpent() {
+    return spent;
+  } 
+  
+  public String getCategory() {
+    return category;
+  }
+
+  @Override
+  public String toString() {
+      return "Category: " + category + " spent: " + spent + " limit: " + limit;
+  }
+  
 }
