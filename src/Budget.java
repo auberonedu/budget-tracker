8ff3b46dba3125b6244c41bdfb66e899d21e99e8
kriseattle <notbbbfamous@gmail.com>
2025-04-29 15:07:35 -0700

    Added get method, totalLimit and totalSpent

--- src/Budget.java
+++ src/Budget.java
@@ -9,6 +9,7 @@ public class Budget {
     // A constructor that takes no arguments and initializes the Map to an empty HashMap.
     public Budget(){
         this.budgetMap = new HashMap<>();
+
     }
 
     // A public method "add" that adds a category to the Map with the name being the key and the value being the BudgetCategory. It should take a single BudgetCategory as an argument.
@@ -17,13 +18,35 @@ public class Budget {
     }
 
     //A public method "get" that gets a category from the Map by name. It should take a String as an argument and return a BudgetCategory. If the category is not present, it should return null.
-    
+    public BudgetCategory get(String categoryName){
+        if (this.budgetMap.containsKey(categoryName)){
+        return this.budgetMap.get(categoryName);
+        } else{
+            return null;
+        }
+
+    }
 
     //A public method totalLimit that takes no arguments and returns a double representing the total limit across all the categories.
+    public double totalLimit(){
+        double total = 0;
+        for (BudgetCategory category : budgetMap.values()){
+            total += category.getLimit();
+        }
+        return total;
+    }
 
 
     // A public method totalSpent that takes no arguments and returns a double representing the total spend across all the categories.
+    public double totalSpent(){
+        double total = 0;
+        for(BudgetCategory category : budgetMap.values()){
+            total += category.getSpent();
+        }
+        return total;
+    }
 
 
     // A public method remainder that takes no arguments and returns a double representing how much money is left in the budget. If the budegt is over the limit, the returned value should be negative.
 }
+
