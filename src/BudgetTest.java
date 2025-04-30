c99daa2c7fc5f98a76c938bf761118a7567a7a4e
Johncarlo Perez <johncarloperez@Johncarlos-MacBook-Pro.local>
2025-04-29 21:12:46 -0700

    added junit test for budget method calculations

--- src/BudgetTest.java
+++ src/BudgetTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class BudgetTest {
     @Test
     void checkingTotalLimit() {
-        Budget budget = new Budget();
+        Budget budget = new Budget(); // empty obj
         budget.add(new BudgetCategory("Groceries", 500, 400)); // under budget $100
         budget.add(new BudgetCategory("Rent", 1500, 1500)); // balanced $0
         budget.add(new BudgetCategory("Entertainment", 350, 200)); // under budget by $150
@@ -13,6 +13,28 @@ public class BudgetTest {
 
         assertEquals(2350.0, actual);
 
+    }
+    @Test 
+    void checkingTotalSpent() {
+        Budget budget = new Budget();
+        budget.add(new BudgetCategory("Groceries", 500, 400)); // under budget $100
+        budget.add(new BudgetCategory("Rent", 1500, 1500)); // balanced $0
+        budget.add(new BudgetCategory("Entertainment", 350, 200)); // under budget by $150
+
+        double actual = budget.totalSpent();
+
+        assertEquals(2100, actual);
+    }
+    @Test
+    void checkingRemainder() {
+        Budget budget = new Budget();
+        budget.add(new BudgetCategory("Groceries", 500, 400)); // under budget $100
+        budget.add(new BudgetCategory("Rent", 1500, 1500)); // balanced $0
+        budget.add(new BudgetCategory("Entertainment", 350, 200)); // under budget by $150
+
+        double actual = budget.remainder();
+
+        assertEquals(-250, actual);
     }
 
 }
