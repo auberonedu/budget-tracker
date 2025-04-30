b27b9d49a372b59fa5ae9f8ec1a9a73b5805b6ba
Johncarlo Perez <johncarloperez@Johncarlos-MacBook-Pro.local>
2025-04-29 20:07:12 -0700

    added a few methods to add to the empty map and get values from it

--- src/Budget.java
+++ src/Budget.java
@@ -1,3 +1,24 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Budget {
-    
+    private Map<String, BudgetCategory> categories = new HashMap<>();
+
+    public Budget() { 
+        this.categories = new HashMap<>();
+    }
+    public void add(BudgetCategory othCategory) {
+        String categoryName = othCategory.getCategory();
+        categories.put(categoryName, othCategory);
+    }
+    public BudgetCategory get(String categoryString) {
+        return categories.get(categoryString);
+    }
+    public double totalLimit() {
+        double count = 0.0;
+        for(BudgetCategory budgetCategory : categories.values()) {
+            count += budgetCategory.getLimit();
+        }
+        return count;
+    }
 }
