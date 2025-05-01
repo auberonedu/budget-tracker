244232e26aa888d90942645c38cf7e2ff27d0f0b
Kiyum-06 <kiyum1406@gmail.com>
2025-05-01 12:35:15 -0700

    Created instance variables, constructor, getters and a toString

--- src/BudgetCategory.java
+++ src/BudgetCategory.java
@@ -1,3 +1,35 @@
 public class BudgetCategory {
-    
-}
+    // instance variables
+    private String name;
+    private double limit;
+    private double spend;
+
+    // Constructor
+    public BudgetCategory(String name, double limit, double spend) {
+        this.name = name;
+        this.limit = limit;
+        this.spend = spend;
+    } // end of constructor
+
+    // adding getters
+    public String getName() {
+        return name;
+    }
+
+    public double getLimit() {
+        return limit;
+    }
+
+    public double getSpend() {
+        return spend;
+    }
+
+    // adding a toString
+    @Override
+    public String toString() {
+        return "Category: " + name +
+                "\nBudgeted: $" + limit +
+                "\nSpent: $" + spend;
+    }
+
+} // end of class
