b54e65da6bc8edae74d5fd11a98768dedebaebe9
Kiyum-06 <kiyum1406@gmail.com>
2025-05-01 13:21:36 -0700

    tested ToString

--- src/BudgetAppTest.java
+++ src/BudgetAppTest.java
@@ -2,5 +2,16 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BudgetAppTest {
-    
+    @Test
+    public void testToString() {
+        // Arrange
+        BudgetCategory diningOut = new BudgetCategory("Dining Out", 120.00, 185.00);
+
+        // Act
+        String actual = diningOut.toString();
+
+        // Assert
+        String expected = "Category: Dining Out\nBudget: $120.0\nSpent: $185.0";
+        assertEquals(expected, actual);
+    }
 }
