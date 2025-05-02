67af56da007f7910e0fe002c508284edddd5939f
Kiyum-06 <kiyum1406@gmail.com>
2025-05-02 14:57:37 -0700

    added 3 tests

--- src/BudgetAppTest.java
+++ src/BudgetAppTest.java
@@ -14,4 +14,43 @@ public class BudgetAppTest {
         String expected = "Category: Dining Out\nBudget: $120.0\nSpent: $185.0";
         assertEquals(expected, actual);
     }
+
+    @Test
+    public void testCompareToUnderVsOverBudget() {
+        // Arrange
+        BudgetCategory groceries = new BudgetCategory("Groceries", 500, 495);
+        BudgetCategory eatingOut = new BudgetCategory("Eating Out", 100, 200);
+
+        // Act
+        int actual = groceries.compareTo(eatingOut);
+
+        // Assert
+        assertEquals(-1, actual);
+    }
+
+    @Test
+    public void testCompareToUnderVsMoreUnderBudget() {
+        // Arrange
+        BudgetCategory groceries = new BudgetCategory("Groceries", 500, 495);
+        BudgetCategory entertainment = new BudgetCategory("Entertainment", 80, 50);
+
+        // Act
+        int actual = groceries.compareTo(entertainment);
+
+        // Assert
+        assertEquals(1, actual);
+    }
+
+    @Test
+    public void testCompareToEqualBudget() {
+        // Arrange
+        BudgetCategory rent1 = new BudgetCategory("Rent", 1000, 1000);
+        BudgetCategory rent2 = new BudgetCategory("Rent", 1000, 1000);
+
+        // Act
+        int actual = rent1.compareTo(rent2);
+
+        // Assert
+        assertEquals(0, actual);
+    }
 }
