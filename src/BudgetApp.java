6f43c06ac0451cd02881d8e9e1187e1633bc0436
Masih <masihrahmani87@gmail.com>
2025-05-03 23:22:40 -0700

    Add support for reading budget data from a file passed as a command-line argument

--- src/BudgetApp.java
+++ src/BudgetApp.java
@@ -2,37 +2,44 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
-
+import java.io.*;
 
 public class BudgetApp {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         // BudgetCategory groceries = new BudgetCategory("Groceries", 500, 401);
         // System.out.println(groceries);
 
-        List<BudgetCategory> categories = new ArrayList<>();
-        Scanner scan = new Scanner(System.in);
+        if (args.length == 0) {
+            System.out.println("Please provide a filename.");
+            return;
+        }
 
-        while(scan.hasNextLine()) {
-            
+        Scanner scan = new Scanner(new File(args[0]));
+        List<BudgetCategory> categories = new ArrayList<>();
+
+        
+        while (scan.hasNextLine()) {
             String category = scan.nextLine();
             double limit = scan.nextDouble();
             double spent = scan.nextDouble();
 
             // Consume \n after spent input 
-            if(scan.hasNextLine()) scan.nextLine();
+            if (scan.hasNextLine()) scan.nextLine();
 
             BudgetCategory bc = new BudgetCategory(category, limit, spent);
             categories.add(bc);
-
         }
 
+     
         Collections.sort(categories);
         Collections.reverse(categories);
 
+        
         for (BudgetCategory bc : categories) {
             System.out.println(bc);
         }
 
+        
         int total = budgetDifference(categories);
         System.out.println("Total budget difference: " + total);
     }
@@ -50,19 +57,12 @@ public class BudgetApp {
      * @return the total amount over/under budget
      */
     public static int budgetDifference(List<BudgetCategory> categories) {
-        // TODO: You will implement this method in Wave 5
-        // Note that this method SHOULD NOT have a print statement.
-        // It should instead return the value.
         int totalDifference = 0;
 
         for (BudgetCategory category : categories) {
             totalDifference += category.getSpent() - category.getLimit();
         }
-    
+
         return totalDifference;
     }
-    
-
-        
-    
 }
