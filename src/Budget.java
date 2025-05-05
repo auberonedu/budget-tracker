61add07d5e00ef4170c367b8e992aa65b9e67abd
sandeersson <sandra.anderstrom@gmail.com>
2025-05-04 23:42:09 -0700

    Added constructor

--- src/Budget.java
+++ src/Budget.java
@@ -1,5 +1,11 @@
+import java.util.HashMap;
 import java.util.Map;
 
 public class Budget {
     private Map<String, BudgetCategory> categories;
+
+    // Constructor
+    public Budget() {
+        categories = new HashMap<>();
+    }
 }
