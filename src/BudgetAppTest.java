a89c4332fa965a9aa04da80ca7daecfee96cd30e
Bluenzo <80460870+Bluenzo@users.noreply.github.com>
2025-05-14 14:06:40 -0700

    Wave 2, toString testing

--- src/BudgetAppTest.java
+++ src/BudgetAppTest.java
@@ -2,5 +2,15 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BudgetAppTest {
+    //Test toString 
+    @Test
+    public void testToString() {
+        BudgetCategory groceries = new BudgetCategory("Groceries", 500, 401);
+
+        String actual = groceries.toString();
+
+        String expected = "Groceries : Budget Limit $500.00, Spent $401.00";
+        assertEquals(expected, actual, "toString method did not return expected output!");
+    }
     
 }
