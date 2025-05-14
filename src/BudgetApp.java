2e52917fb985390c190903ee01314eb1fa9bc33e
Bluenzo <80460870+Bluenzo@users.noreply.github.com>
2025-05-14 14:18:32 -0700

    Wave 3, Budget Refactor

--- src/BudgetApp.java
+++ src/BudgetApp.java
@@ -1,52 +1,56 @@
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class BudgetApp {
     public static void main(String[] args) {
 
-        BudgetCategory groceries = new BudgetCategory("Groceries", 500, 401);
+        // BudgetCategory groceries = new BudgetCategory("Groceries", 500, 401);
 
     
-        System.out.println("Category: " + groceries.getName());
-        System.out.println("Budget Limit: $" + groceries.getLimit());
-        System.out.println("Amount Spent: $" + groceries.getSpent());
+        // System.out.println("Category: " + groceries.getName());
+        // System.out.println("Budget Limit: $" + groceries.getLimit());
+        // System.out.println("Amount Spent: $" + groceries.getSpent());
 
-        System.out.println(groceries);
+        // System.out.println(groceries);
 
-    //     Scanner scan = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
 
-    //     while(scan.hasNextLine()) {
-    //         String category = scan.nextLine();
+        List<BudgetCategory> categories = new ArrayList<>();
 
-    //         double limit = scan.nextDouble();
-    //         double spent = scan.nextDouble();
+        while(scan.hasNextLine()) {
+            String category = scan.nextLine();
 
-    //         // Consume \n after spent input 
-    //         if(scan.hasNextLine()) scan.nextLine();
+            double limit = scan.nextDouble();
+            double spent = scan.nextDouble();
 
-    //         String limitString = String.format("$%.2f", limit);
-    //         String spentString = String.format("$%.2f", spent);
-    //         System.out.println("The budget limit for " + category + " was: " + limitString + 
-    //                            " but the actual spend was " + spentString);
-    //     }
-    // }
+            // Consume \n after spent input 
+            if(scan.hasNextLine()) scan.nextLine();
 
-    // /**
-    //  * Returns overall how much over/under budget a person is given a list of their
-    //  * categories.
-    //  * 
-    //  * This should be the sum of how much over/under budget each individual category is.
-    //  * 
-    //  * If the person is under budget, the result will be negative. If they are over budget, the
-    //  * result will be positive.
-    //  * 
-    //  * @param categories the budget categories with the spend
-    //  * @return the total amount over/under budget
-    //  */
-    // public static int budgetDifference(List<BudgetCategory> categories) {
-    //     // TODO: You will implement this method in Wave 5
-    //     // Note that this method SHOULD NOT have a print statement.
-    //     // It should instead return the value.
-    //     return -1;
+            categories.add(new BudgetCategory(category, limit, spent));
+        }
+        
+            for (BudgetCategory bc : categories){
+                System.out.println(bc);
+            }
+    }
+
+    /**
+     * Returns overall how much over/under budget a person is given a list of their
+     * categories.
+     * 
+     * This should be the sum of how much over/under budget each individual category is.
+     * 
+     * If the person is under budget, the result will be negative. If they are over budget, the
+     * result will be positive.
+     * 
+     * @param categories the budget categories with the spend
+     * @return the total amount over/under budget
+     */
+    public static int budgetDifference(List<BudgetCategory> categories) {
+        // TODO: You will implement this method in Wave 5
+        // Note that this method SHOULD NOT have a print statement.
+        // It should instead return the value.
+        return -1;
      }
 }
