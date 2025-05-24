41f58fbc5489faab949915d9ca66ed60b0766f75
Jessica H <jessicahebert75@gmail.com>
2025-05-24 10:57:18 -0700

    Added a test to test to string

--- src/BudgetCategoryTest.java
+++ src/BudgetCategoryTest.java
@@ -1,6 +1,14 @@
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BudgetCategoryTest {
+    @Test
+    public void testTwoString(){
+    BudgetCategory t2s = new BudgetCategory("groceries",500.00,401.00);
     
+    String results = t2s.toString();
+
+    assertEquals("groceries 401.0 500.0", results);
+    }
 }
