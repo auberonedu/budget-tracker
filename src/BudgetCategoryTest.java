0f6801329d11a29ecb68bb6dfd41b92807cb7636
Jessica H <jessicahebert75@gmail.com>
2025-05-29 18:03:59 -0700

    added test for compareTo

--- src/BudgetCategoryTest.java
+++ src/BudgetCategoryTest.java
@@ -11,4 +11,34 @@ public class BudgetCategoryTest {
 
     assertEquals("groceries 401.0 500.0", results);
     }
+
+    @Test
+    public void testUnderCompareTo(){
+        BudgetCategory groceries = new BudgetCategory("groceries",500.00, 249);
+        BudgetCategory rent = new BudgetCategory("Rent", 2500, 1500);
+
+        int results = groceries.compareTo(rent);
+
+        assertEquals(1,results);
+    }
+    @Test
+
+    public void testIfEqual () {
+        BudgetCategory sameDifference1 = new BudgetCategory("groceries", 600, 50);    
+        BudgetCategory sameDifference2 = new BudgetCategory("car", 600, 50);
+    
+        int results = sameDifference1.compareTo(sameDifference2);
+
+        assertEquals(0, results);
+    }
+    @Test
+    public void testOverBudget (){
+        BudgetCategory groceries = new BudgetCategory("Groceries", 100, 300);
+        BudgetCategory bills = new BudgetCategory("Bills", 1500, 1501);
+
+        int results = groceries.compareTo(bills);
+
+        assertEquals(1, results); 
+
+    }
 }
